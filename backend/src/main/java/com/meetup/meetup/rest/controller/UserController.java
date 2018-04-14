2428a4f89baba8a594bd5113b9d90272ea8dcf34
drerek <adrerek@gmail.com>
2018-04-15 00:26:56 +0300

    add login form, reformat code, add dao interfaces

--- backend/src/main/java/com/meetup/meetup/rest/controller/UserController.java
+++ backend/src/main/java/com/meetup/meetup/rest/controller/UserController.java
@@ -6,43 +6,51 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import sun.misc.Request;
 
+import javax.json.Json;
+import javax.json.JsonBuilderFactory;
+import javax.json.JsonObject;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @RestController
 @RequestMapping("/api")
 public class UserController {
 
-    @PostMapping("/register")
+    @PostMapping(path = "/register", consumes = "application/json", produces = "application/json")
     @ResponseStatus(HttpStatus.CREATED)
-    public void registerAccount(@Valid @RequestBody User user) {
+    public String registerAccount(@Valid @RequestBody User user) {
         OracleUserDAO oracleUserDAO = new OracleUserDAO();
-        if (null == oracleUserDAO.findUserByUserName(user.getUsername())) {
-            //return thisUserName is busy
 
+        if (null == oracleUserDAO.findUserByUserName(user.getUsername())) {
+        return Json.createObjectBuilder().add("error", "This username is busy").build().toString();
         }
         if (null == oracleUserDAO.findUserByMail(user.getEmail())) {
-            //return this email is busy
+            return Json.createObjectBuilder().add("error", "This email is busy").build().toString();
         }
         if (oracleUserDAO.insertUser(user) == -1) {
-            //return false;
+            return Json.createObjectBuilder().add("error", "Something went wrong").build().toString();
         }
+        return Json.createObjectBuilder().add("success", "Success").build().toString();
     }
 
     @PostMapping("/login")
     @ResponseStatus(HttpStatus.CREATED)
-    public void loginAccount(@Valid @RequestBody User user) {
+    public String loginAccount(@Valid @RequestBody User user) {
         OracleUserDAO oracleUserDAO = new OracleUserDAO();
+        String value = Json.createObjectBuilder().add("error", "Login or password incorrect").build().toString();
         if (null == oracleUserDAO.findUserByUserName(user.getUsername())) {
-        //return error
+            return value;
         }
         User requestedUser = oracleUserDAO.findUserByMail(user.getUsername());
         if (!requestedUser.getPassword().equals(user.getPassword())) {
-        //return error
+            return value;
         }
         Cookie loginCookie = new Cookie("userName", requestedUser.getUsername());
+        return "endfile";
         //checking email and pass then log in user
     }
 }
