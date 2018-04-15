5bfe6cbbe94674db4df34cbc6556e210f2cee1a0
drerek <adrerek@gmail.com>
2018-04-15 18:15:48 +0300

    Fit for other DAO, deleting old DAO, some modifications;

--- backend/src/main/java/com/meetup/meetup/rest/controller/UserController.java
+++ backend/src/main/java/com/meetup/meetup/rest/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.meetup.meetup.rest.controller;
 
-import com.meetup.meetup.DAO.OracleUserDAO;
 import com.meetup.meetup.rest.model.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -12,24 +11,24 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("/api")
 public class UserController {
-    private static final int TABLE_NUMBER = 5;
+//    private static final int TABLE_NUMBER = 5;
 
     @PostMapping(path = "/register", consumes = "application/json", produces = "application/json")
     @ResponseStatus(HttpStatus.CREATED)
     public String registerAccount(@Valid @RequestBody User user) {
-        OracleUserDAO oracleUserDAO = new OracleUserDAO();
+        UserDaoImpl userDaoImpl = new UserDaoImpl();
 
-        if (null == oracleUserDAO.findUserByUserName(user.getUsername())) { //checking if user exist in system
+        if (null == userDaoImpl.findByLogin(user.getLogin())) { //checking if user exist in system
             return Json.createObjectBuilder().add("error", "This username is busy").build().toString();
         }
-        if (null == oracleUserDAO.findUserByMail(user.getEmail())) { //checking if email exist in system
+        if (null == userDaoImpl.findByEmail(user.getEmail())) { //checking if email exist in system
             return Json.createObjectBuilder().add("error", "This email is busy").build().toString();
         }
-        if (oracleUserDAO.insertUser(user) == -1) { //checking adding to DB
+        //user.setUserId("" + System.currentTimeMillis() + TABLE_NUMBER + (int) (Math.random() * 100));
+        if (userDaoImpl.insert(user) == -1) { //checking adding to DB
             return Json.createObjectBuilder().add("error", "Something went wrong").build().toString();
         }
-        user.setUserId("" + System.currentTimeMillis() + TABLE_NUMBER + (int) (Math.random() * 100));
-        oracleUserDAO.insertUser(user);
+
         return Json.createObjectBuilder().add("success", "Success").build().toString();
     }
 
@@ -39,14 +38,14 @@ public class UserController {
 
         String value = Json.createObjectBuilder().add("error", "Login or password incorrect").build().toString();
 
-        OracleUserDAO oracleUserDAO = new OracleUserDAO();
-        User requestedUser = oracleUserDAO.findUserByUserName(user.getUsername());
+        UserDaoImpl userDaoImpl = new UserDaoImpl();
+        User requestedUser = userDaoImpl.findByLogin(user.getLogin());
         if (null == requestedUser) { //checking existence of user in system
             return value;
         }
         if (!requestedUser.getPassword().equals(user.getPassword())) { //checking equals of password
             return value;
         }
-        return Json.createObjectBuilder().add("userId", requestedUser.getUserId()).build().toString();
+        return Json.createObjectBuilder().add("userId", requestedUser.getId()).build().toString();
     }
 }
