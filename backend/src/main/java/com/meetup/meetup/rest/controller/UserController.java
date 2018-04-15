a593fdfdefce5c450fdb94fb7ac4fe85d5db05d6
Olga Kozlova <olgasergeevna0803@gmail.com>
2018-04-15 13:07:30 +0300

    Improved login page to be landing.

--- backend/src/main/java/com/meetup/meetup/rest/controller/UserController.java
+++ backend/src/main/java/com/meetup/meetup/rest/controller/UserController.java
@@ -8,17 +8,19 @@ import org.springframework.web.bind.annotation.*;
 import javax.json.Json;
 import javax.validation.Valid;
 
+
 @RestController
 @RequestMapping("/api")
 public class UserController {
     private static final int TABLE_NUMBER = 5;
+
     @PostMapping(path = "/register", consumes = "application/json", produces = "application/json")
     @ResponseStatus(HttpStatus.CREATED)
     public String registerAccount(@Valid @RequestBody User user) {
         OracleUserDAO oracleUserDAO = new OracleUserDAO();
 
         if (null == oracleUserDAO.findUserByUserName(user.getUsername())) { //checking if user exist in system
-        return Json.createObjectBuilder().add("error", "This username is busy").build().toString();
+            return Json.createObjectBuilder().add("error", "This username is busy").build().toString();
         }
         if (null == oracleUserDAO.findUserByMail(user.getEmail())) { //checking if email exist in system
             return Json.createObjectBuilder().add("error", "This email is busy").build().toString();
@@ -26,7 +28,7 @@ public class UserController {
         if (oracleUserDAO.insertUser(user) == -1) { //checking adding to DB
             return Json.createObjectBuilder().add("error", "Something went wrong").build().toString();
         }
-        user.setUserId(""+System.currentTimeMillis()+TABLE_NUMBER+(int)(Math.random()*100));
+        user.setUserId("" + System.currentTimeMillis() + TABLE_NUMBER + (int) (Math.random() * 100));
         oracleUserDAO.insertUser(user);
         return Json.createObjectBuilder().add("success", "Success").build().toString();
     }
--- frontend/src/main/frontend/src/app/account/login/login.component.ts
+++ frontend/src/main/frontend/src/app/account/login/login.component.ts
@@ -5,7 +5,8 @@ import {AccountService} from "../account.service";
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.component.html'
+  templateUrl: './login.component.html',
+  styleUrls: [ './login.component.css' ]
 })
 export class LoginComponent implements OnInit {
   success: boolean;
