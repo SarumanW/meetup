66662ff988a7474435d8211e7fae57a99dfbbf40
bazyl <svintcov95@gmail.com>
2018-04-21 00:07:59 +0300

    -added lombok annotations

--- backend/src/main/java/com/meetup/meetup/entity/User.java
+++ backend/src/main/java/com/meetup/meetup/entity/User.java
@@ -1,14 +1,14 @@
 package com.meetup.meetup.entity;
 
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
-import java.time.LocalDate;
 
+@Data
 public class User {
 
     @JsonIgnore
@@ -43,128 +43,4 @@ public class User {
     @JsonIgnore
     private String imgPath;
 
-    public User() {
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
-    public String getBirthDay() {
-        return birthDay;
-    }
-
-    public void setBirthDay(String birthDay) {
-        this.birthDay = birthDay;
-    }
-
-    public int getTimeZone() {
-        return timeZone;
-    }
-
-    public void setTimeZone(int timeZone) {
-        this.timeZone = timeZone;
-    }
-
-    public String getImgPath() {
-        return imgPath;
-    }
-
-    public void setImgPath(String imgPath) {
-        this.imgPath = imgPath;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", login='" + login + '\'' +
-                ", password='" + password + '\'' +
-                ", email='" + email + '\'' +
-                ", phone='" + phone + '\'' +
-                ", name='" + name + '\'' +
-                ", lastName='" + lastname + '\'' +
-                ", birthDay=" + birthDay +
-                ", timeZone=" + timeZone +
-                ", imgPath='" + imgPath + '\'' +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        User user = (User) o;
-
-        if (id != user.id) return false;
-        if (!login.equals(user.login)) return false;
-        if (!email.equals(user.email)) return false;
-        if (!name.equals(user.name)) return false;
-        if (!lastname.equals(user.lastname)) return false;
-        return birthDay != null ? birthDay.equals(user.birthDay) : user.birthDay == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id;
-        result = 31 * result + login.hashCode();
-        result = 31 * result + email.hashCode();
-        result = 31 * result + name.hashCode();
-        result = 31 * result + lastname.hashCode();
-        result = 31 * result + (birthDay != null ? birthDay.hashCode() : 0);
-        return result;
-    }
 }
--- backend/src/main/java/com/meetup/meetup/service/vm/Profile.java
+++ backend/src/main/java/com/meetup/meetup/service/vm/Profile.java
@@ -1,9 +1,12 @@
 package com.meetup.meetup.service.vm;
 
 import com.meetup.meetup.entity.User;
+import lombok.Data;
 
 import javax.validation.constraints.Size;
 
+
+@Data
 public class Profile {
 
     private int id;
@@ -21,12 +24,9 @@ public class Profile {
     private String birthDay;
     private String imgPath;
 
-    public Profile(){}
 
     public Profile(String login, String name, String lastname, String password) {
-        this.login = login;
-        this.name = name;
-        this.lastname= lastname;
+        this(name, lastname, login);
         this.password = password;
     }
 
@@ -55,75 +55,4 @@ public class Profile {
         return user;
     }
 
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getBirthDay() {
-        return birthDay;
-    }
-
-    public void setBirthDay(String birthDay) {
-        this.birthDay = birthDay;
-    }
-
-    public String getImgPath() {
-        return imgPath;
-    }
-
-    public void setImgPath(String imgPath) {
-        this.imgPath = imgPath;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
 }
