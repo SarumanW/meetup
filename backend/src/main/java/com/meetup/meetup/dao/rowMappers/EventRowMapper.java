d011d5787860bda69274b46edea2a5be895a0312
Peter Pridorozhny <Peter35128@gmail.com>
2018-04-21 23:34:35 +0300

    fixed null bag in EventRowMapper

--- backend/src/main/java/com/meetup/meetup/dao/rowMappers/EventRowMapper.java
+++ backend/src/main/java/com/meetup/meetup/dao/rowMappers/EventRowMapper.java
@@ -5,17 +5,21 @@ import com.meetup.meetup.entity.EventPeriodicity;
 import com.meetup.meetup.entity.EventType;
 import org.springframework.jdbc.core.RowMapper;
 
+import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 
 public class EventRowMapper implements RowMapper<Event> {
     @Override
     public Event mapRow(ResultSet resultSet, int i) throws SQLException {
         Event event = new Event();
+        Timestamp date;
 
         event.setEventId(resultSet.getInt("EVENT_ID"));
         event.setName(resultSet.getString("NAME"));
-        event.setEventDate(resultSet.getTimestamp("EVENT_DATE").toString());
+        date = resultSet.getTimestamp("EVENT_DATE");
+        event.setEventDate(date == null ? null : date.toString());
         event.setDescription(resultSet.getString("DESCRIPTION"));
         event.setPeriodicityId(resultSet.getInt("PERIODICITY_ID"));
         switch (resultSet.getString("PERIODICITY_NAME")) {
