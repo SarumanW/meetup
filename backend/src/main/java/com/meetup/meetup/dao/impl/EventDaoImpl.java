72837f80834b6753e1ce47d4545ccfd3700b944c
Peter Pridorozhny <Peter35128@gmail.com>
2018-04-22 19:30:28 +0300

    added getRole and create event with user dependency

--- backend/src/main/java/com/meetup/meetup/dao/impl/EventDaoImpl.java
+++ backend/src/main/java/com/meetup/meetup/dao/impl/EventDaoImpl.java
@@ -3,6 +3,7 @@ package com.meetup.meetup.dao.impl;
 import com.meetup.meetup.dao.EventDao;
 import com.meetup.meetup.dao.rowMappers.EventRowMapper;
 import com.meetup.meetup.entity.Event;
+import com.meetup.meetup.entity.Role;
 import com.meetup.meetup.entity.User;
 import com.meetup.meetup.exception.DatabaseWorkException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,6 +31,8 @@ public class EventDaoImpl implements EventDao {
     @Autowired
     @Qualifier("jdbcTemplate")
     private JdbcTemplate jdbcTemplate;
+
+    private final int ownerId = 1;
 
 
     @Override
@@ -74,6 +77,54 @@ public class EventDaoImpl implements EventDao {
             throw new DatabaseWorkException();
         }
         return model;
+    }
+
+    public Event createEvent(Event model, int userId) {
+
+        Event event;
+
+        event = insert(model);
+        insertUserEvent(userId, model.getEventId(), ownerId);
+
+        return event;
+    }
+
+    public Role getRole(int userId, int eventId) {
+
+        Role role = null;
+
+        try {
+            String roleString = jdbcTemplate.queryForObject(
+                    env.getProperty("role.getRole"), new Object[] { userId, eventId },
+                    String.class);
+
+            role = "OWNER".equals(roleString) ? Role.OWNER : Role.PARTICIPANT;
+        } catch (DataAccessException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return role;
+    }
+
+    private int insertUserEvent(int userId, int eventId, int roleId) {
+
+
+        SimpleJdbcInsert simpleJdbcInsert = new SimpleJdbcInsert(jdbcTemplate.getDataSource())
+                .withTableName("USER_EVENT");
+
+        Map<String, Object> parameters = new HashMap<String, Object>();
+        parameters.put("USER_ID", userId);
+        parameters.put("EVENT_ID", eventId);
+        parameters.put("ROLE_ID", roleId);
+
+        try {
+            simpleJdbcInsert.execute(parameters);
+        } catch (DataAccessException e) {
+            System.out.println(e.getMessage());
+            throw new DatabaseWorkException();
+        }
+
+        return roleId;
     }
 
     @Override
