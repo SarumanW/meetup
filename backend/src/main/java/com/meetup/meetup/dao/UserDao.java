c93b2e888ad75bea2fdb28d27727266674c8793b
AAMSLieutenant <rst1991@ukr.net>
2018-04-23 19:26:36 +0300

    Updated UserDAO for geting user's friends

--- backend/src/main/java/com/meetup/meetup/dao/UserDao.java
+++ backend/src/main/java/com/meetup/meetup/dao/UserDao.java
@@ -7,7 +7,17 @@ import java.util.List;
 public interface UserDao extends Dao<User> {
 
     User findByLogin(String login);
+
     User findByEmail(String email);
-    List<Integer> friendsIds(String id);
+
     boolean updatePassword(User user);
+
+    int confirmFriend(int user_id, int friend_id);
+
+    int deleteFriend(int user_id, int friend_id);
+
+    List<User> getFriendsRequests(int user_id);
+
+    List<User> getFriends(int user_id);
+
 }
