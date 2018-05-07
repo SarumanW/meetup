491981d70369703a4ad38b1484d9d4028fff784b
drerek <adrerek@gmail.com>
2018-05-07 21:45:16 +0300

    some fixes

--- backend/src/main/java/com/meetup/meetup/rest/controller/ItemController.java
+++ backend/src/main/java/com/meetup/meetup/rest/controller/ItemController.java
@@ -1,6 +1,7 @@
 package com.meetup.meetup.rest.controller;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.TextNode;
 import com.meetup.meetup.entity.Item;
 import com.meetup.meetup.entity.ItemPriority;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import com.meetup.meetup.service.ItemService;
 
 import javax.validation.Valid;
+import java.io.IOException;
 
 @RestController
 @RequestMapping(path = "/api/item")
@@ -43,8 +45,9 @@ public class ItemController {
         log.debug("Trying to save item '{}'", item);
         Item addedItem = itemService.addItem(item);
 
-        log.debug("Trying to save item with id '{}' to user wish list", addedItem.getItemId());
-        itemService.addItemToUserWishList(addedItem.getItemId(), item.getPriority());
+        // TODO: 07.05.2018  
+//        log.debug("Trying to save item with id '{}' to user wish list", addedItem.getItemId());
+//        addedItem = itemService.addItemToUserWishList(addedItem.getItemId(), item);
 
         log.debug("Send response body saved item '{}' and status CREATED", addedItem);
         return new ResponseEntity<>(addedItem, HttpStatus.CREATED);
@@ -52,9 +55,11 @@ public class ItemController {
 
     @PostMapping("/{id}/add")
     public @ResponseBody
-    ResponseEntity<Item> addItemToUserWishList(@PathVariable int id, @Valid @RequestBody ItemPriority itemPriority) {
+    ResponseEntity<Item> addItemToUserWishList(@PathVariable int id, @Valid @RequestBody Item item) {
         log.debug("Trying to add item with id '{}' to user wish list", id);
-        itemService.addItemToUserWishList(id, itemPriority);
+
+        Item addedItem = itemService.addItemToUserWishList(id, item);
+        log.debug("Item was added with id '{}'", addedItem.getItemId());
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
@@ -66,6 +71,26 @@ public class ItemController {
 
         log.debug("Send response status OK");
         return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @PostMapping("/{id}/like")
+    public @ResponseBody
+    ResponseEntity<Item> addLike(@PathVariable int id) {
+        log.debug("Trying to add like to item with id '{}'", id);
+
+        Item likedItem = itemService.addLike(id);
+        log.debug("Item was added with id '{}'", id);
+        return new ResponseEntity<>(likedItem, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}/like")
+    public @ResponseBody
+    ResponseEntity<Item> removeLike(@PathVariable int id) {
+        log.debug("Trying to remove like from item with id '{}'", id);
+
+        Item unlikedItem = itemService.removeLike(id);
+        log.debug("Item was added with id '{}'", id);
+        return new ResponseEntity<>(unlikedItem, HttpStatus.OK);
     }
 
     @PutMapping
