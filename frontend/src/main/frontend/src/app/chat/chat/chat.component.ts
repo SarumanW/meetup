6a08f315bfae1b2c77926b4269338f3d822b7284
Olga Kozlova <olgasergeevna0803@gmail.com>
2018-05-08 23:22:06 +0300

    Improved chat view.

--- frontend/src/main/frontend/src/app/chat/chat/chat.component.ts
+++ frontend/src/main/frontend/src/app/chat/chat/chat.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, Renderer2, RendererFactory2} from '@angular/core';
+import {Component, OnInit, Renderer2, RendererFactory2, ViewEncapsulation} from '@angular/core';
 import * as Stomp from 'stompjs';
 import * as SockJS from 'sockjs-client';
 import * as $ from 'jquery';
@@ -16,14 +16,8 @@ export class ChatComponent implements OnInit {
   state: string = "chat";
   messageText: string;
   profile: Profile;
-  color;
 
   isButtonHidden: boolean = false;
-
-  colorss = [
-    '#2196F3', '#32c787', '#00BCD4', '#ff5652',
-    '#ffc107', '#ff85af', '#FF9800', '#39bbb0'
-  ];
 
   constructor() {
 
@@ -57,23 +51,51 @@ export class ChatComponent implements OnInit {
 
         let message = JSON.parse(payload.body);
         let messageElement;
+        let color;
 
         if (message.type === 'JOIN') {
           message.content = message.sender + ' joined!';
-          messageElement = `<li class="event-message">
-    <p>${message.content}</p>
+          messageElement = `<li class="event-message" style="width: 100%;
+  text-align: center;
+  clear: both;">
+    <p style="color: #777;
+  font-size: 14px;
+  word-wrap: break-word;">${message.content}</p>
 </li>`;
         } else if (message.type === 'LEAVE') {
           message.content = message.sender + ' left!';
-          messageElement = `<li class="event-message">
-    <p>${message.content}</p>
+          messageElement = `<li class="event-message" style="width: 100%;
+  text-align: center;
+  clear: both;">
+    <p style="color: #777;
+  font-size: 14px;
+  word-wrap: break-word;">${message.content}</p>
 </li>`;
         } else {
-          this.color = colors[0];
-          messageElement = `<li class="chat-message">
-    <i [style.backround-color]='color'>${message.sender[0]}</i>
-    <span>${message.sender}</span>
-    <p>${message.content}</p>
+          let index = Math.floor(Math.random() * colors.length);
+
+          let color = colors[index];
+
+          messageElement = `<li class="chat-message" style="padding-left: 68px;
+  position: relative;">
+    <i style="position: absolute;
+  width: 42px;
+  height: 42px;
+  overflow: hidden;
+  left: 10px;
+  display: inline-block;
+  vertical-align: middle;
+  font-size: 18px;
+  line-height: 42px;
+  color: #fff;
+  text-align: center;
+  border-radius: 50%;
+  font-style: normal;
+  text-transform: uppercase;
+  background-color:${color}";>${message.sender[0]}</i>
+    <span style="color: #333;
+  font-weight: 600;">${message.sender}</span>
+    <p style="color: #43464b;">${message.content}</p>
 </li>`;
         }
 
@@ -97,16 +119,5 @@ export class ChatComponent implements OnInit {
     this.stompClient.send("/app-chat/send/message", {}, JSON.stringify(chatMessage));
 
     $('#message').val('');
-  }
-
-  getAvatarColor(messageSender): number {
-    let hash = 0;
-
-    for (let i = 0; i < messageSender.length; i++) {
-      hash = 31 * hash + messageSender.charCodeAt(i);
-    }
-
-    let index = Math.abs(hash % this.colorss.length);
-    return index;
   }
 }
