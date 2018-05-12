d339577302337db542c0398061207e72ae890520
Denys Yashchuk <denys.yashchuk@gmail.com>
2018-05-12 17:33:53 +0300

    #115 Fixed sending of event plans to email #115

--- backend/src/main/java/com/meetup/meetup/service/PdfCreatService.java
+++ backend/src/main/java/com/meetup/meetup/service/PdfCreatService.java
@@ -11,7 +11,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.util.List;
 import java.util.stream.Stream;
-
+//ng build --prod
 @Service
 public class PdfCreatService {
 
