88cc63ea9c18309a8395f350316a6a5c93b42955
Alex Smirnow <salexm948@ukr.net>
2018-05-14 18:37:48 +0300

    Implemented MessageRowMapper

--- backend/src/main/java/com/meetup/meetup/dao/rowMappers/MessageRowMapper.java
+++ backend/src/main/java/com/meetup/meetup/dao/rowMappers/MessageRowMapper.java
@@ -6,6 +6,7 @@ import org.springframework.jdbc.core.RowMapper;
 import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,8 +14,17 @@ import static com.meetup.meetup.keys.Key.*;
 
 public class MessageRowMapper implements RowMapper<Message> {
     @Override
-    public Message mapRow(ResultSet rs, int rowNum) throws SQLException {
-        return null;
+    public Message mapRow(ResultSet resultSet, int rowNum) throws SQLException {
+        Message message = new Message();
+
+        message.setMessageId(resultSet.getInt(MESSAGE_MESSAGE_ID));
+        message.setSenderId(resultSet.getInt(MESSAGE_SENDER_ID));
+        message.setChatId(resultSet.getInt(MESSAGE_CHAT_ID));
+        message.setText(resultSet.getString(MESSAGE_TEXT));
+        Timestamp date = resultSet.getTimestamp(EVENT_EVENT_DATE);
+        message.setMessageDate(date == null ? null : date.toString());
+
+        return message;
     }
 
     public static Map<String, Object> paramsMapper(Message message) {
