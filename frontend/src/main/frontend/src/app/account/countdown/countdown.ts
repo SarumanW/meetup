3e4f9d781dec33c6a387ab91b13e9292a0915952
alexanaw <natasha.bojchenko@gmail.com>
2018-05-20 11:55:08 +0300

    fixed negative values on CountDown

--- frontend/src/main/frontend/src/app/account/countdown/countdown.ts
+++ frontend/src/main/frontend/src/app/account/countdown/countdown.ts
@@ -38,83 +38,83 @@ export class CountDown {
       this.reached.next(now);
     }
 
-    let lastUnit = this.units[this.units.length - 1],
-      unitConstantForMillisecs = {
-        year: (((1000 * 60 * 60 * 24 * 7) * 4) * 12),
-        month: ((1000 * 60 * 60 * 24 * 7) * 4),
-        weeks: (1000 * 60 * 60 * 24 * 7),
-        days: (1000 * 60 * 60 * 24),
-        hours: (1000 * 60 * 60),
-        minutes: (1000 * 60),
-        seconds: 1000
-      },
-      unitsLeft = {},
-      returnText = '',
-      returnNumbers = '',
-      totalMillisecsLeft = dateDifference,
-      i,
-      unit: any;
+    if (dateDifference <= 0) {
+      this.units = null;
+    }
 
-    for (i in this.units) {
-      if (this.units.hasOwnProperty(i)) {
+    if (this.units !== null) {
+      let lastUnit = this.units[this.units.length - 1],
+        unitConstantForMillisecs = {
+          year: (((1000 * 60 * 60 * 24 * 7) * 4) * 12),
+          month: ((1000 * 60 * 60 * 24 * 7) * 4),
+          weeks: (1000 * 60 * 60 * 24 * 7),
+          days: (1000 * 60 * 60 * 24),
+          hours: (1000 * 60 * 60),
+          minutes: (1000 * 60),
+          seconds: 1000
+        },
+        unitsLeft = {},
+        returnText = '',
+        returnNumbers = '',
+        totalMillisecsLeft = dateDifference,
+        i,
+        unit: any;
 
-        unit = this.units[i].trim();
-        if (unitConstantForMillisecs[unit.toLowerCase()] === false) {
-          //$interval.cancel(countDownInterval);
-          throw new Error('Cannot repeat unit: ' + unit);
 
+      for (i in this.units) {
+        if (this.units.hasOwnProperty(i)) {
+
+          unit = this.units[i].trim();
+          if (unitConstantForMillisecs[unit.toLowerCase()] === false) {
+            //$interval.cancel(countDownInterval);
+            throw new Error('Cannot repeat unit: ' + unit);
+
+          }
+          if (unitConstantForMillisecs.hasOwnProperty(unit.toLowerCase()) === false) {
+            throw new Error('Unit: ' + unit + ' is not supported. Please use following units: year, month, weeks, days, hours, minutes, seconds, milliseconds');
+          }
+
+          unitsLeft[unit] = totalMillisecsLeft / unitConstantForMillisecs[unit.toLowerCase()];
+
+          if (lastUnit === unit) {
+            unitsLeft[unit] = Math.ceil(unitsLeft[unit]);
+          } else {
+            unitsLeft[unit] = Math.floor(unitsLeft[unit]);
+          }
+
+          totalMillisecsLeft -= unitsLeft[unit] * unitConstantForMillisecs[unit.toLowerCase()];
+          unitConstantForMillisecs[unit.toLowerCase()] = false;
+
+          returnNumbers += ' ' + unitsLeft[unit] + ' | ';
+          returnText += ' ' + unit;
         }
-        if (unitConstantForMillisecs.hasOwnProperty(unit.toLowerCase()) === false) {
-          throw new Error('Unit: ' + unit + ' is not supported. Please use following units: year, month, weeks, days, hours, minutes, seconds, milliseconds');
-        }
-
-        unitsLeft[unit] = totalMillisecsLeft / unitConstantForMillisecs[unit.toLowerCase()];
-
-        if (lastUnit === unit) {
-          unitsLeft[unit] = Math.ceil(unitsLeft[unit]);
-        } else {
-          unitsLeft[unit] = Math.floor(unitsLeft[unit]);
-        }
-
-        totalMillisecsLeft -= unitsLeft[unit] * unitConstantForMillisecs[unit.toLowerCase()];
-        unitConstantForMillisecs[unit.toLowerCase()] = false;
-
-        returnNumbers += ' ' + unitsLeft[unit] + ' | ';
-        returnText += ' ' + unit;
       }
+
+      if (this.text === null || !this.text) {
+        this.text = {
+          Year: ' Year',
+          Month: ' Month',
+          Weeks: ' Weeks',
+          Days: ' Days',
+          Hours: ' Hours',
+          Minutes: ' Minutes',
+          Seconds: ' Seconds',
+          MilliSeconds: ' Milliseconds'
+        };
+      }
+
+      this.displayString = returnText
+        .replace(' Year', this.text.Year + ' | ')
+        .replace(' Month', this.text.Month + ' | ')
+        .replace(' Weeks', this.text.Weeks + ' | ')
+        .replace(' Days', this.text.Days + ' | ')
+        .replace(' Hours', this.text.Hours + ' | ')
+        .replace(' Minutes', this.text.Minutes + ' | ')
+        .replace(' Seconds', this.text.Seconds);
+
+      this.displayNumbers = returnNumbers.split('|');
+      this.display = this.displayString.split('|');
     }
-
-    if (this.text === null || !this.text) {
-      this.text = {
-        Year: ' Year',
-        Month: ' Month',
-        Weeks: ' Weeks',
-        Days: ' Days',
-        Hours: ' Hours',
-        Minutes: ' Minutes',
-        Seconds: ' Seconds',
-        MilliSeconds: ' Milliseconds'
-      };
-    }
-
-    this.displayString = returnText
-      .replace(' Year', this.text.Year + ' | ')
-      .replace(' Month', this.text.Month + ' | ')
-      .replace(' Weeks', this.text.Weeks + ' | ')
-      .replace(' Days', this.text.Days + ' | ')
-      .replace(' Hours', this.text.Hours + ' | ')
-      .replace(' Minutes', this.text.Minutes + ' | ')
-      .replace(' Seconds', this.text.Seconds);
-
-    this.displayNumbers = returnNumbers.split('|');
-    this.display = this.displayString.split('|');
-
-if (this.units==='0'){
-  this.stopTimer();
-}
-
-  }
-  stopTimer(){
-    this.units = null;
   }
 }
+
